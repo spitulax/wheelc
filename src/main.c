9a0b0b3f27a802c13fbba62ee51f5291a93420df
Bintang <bintangadiputrapratama@gmail.com>
2024-03-30 14:45:42 +0700

    Refactor

--- src/main.c
+++ src/main.c
@@ -3,8 +3,20 @@
 #define WINDOW_WIDTH 800
 #define WINDOW_HEIGHT 600
 
-#include "wheel.h"
-#include "color.h"
+// Yes, this is catppuccin mocha
+#define COLOR_BACKGROUND GetColor(0X101020ff)
+#define COLOR_TEXT GetColor(0xcdd6f4ff)
+#define COLOR_BLUE GetColor(0x89b4faff)
+#define COLOR_RED GetColor(0xf38ba8ff)
+
+void wheel_draw(Vector2 center, float radius, float speed, int lines, Color color) {
+  DrawCircleV(center, radius, color);
+  for (int i = 0; i < lines; i++) {
+    float gap = 360.0f/lines*i;
+    float angle = GetTime()*speed + gap;
+    DrawCircleSector(center, radius, angle, angle + 360.0f/lines, 100, ColorBrightness(color, -0.8 + 0.1 * i));
+  }
+}
 
 int main(void)
 {
--- src/wheel.h
+++
@@ -1,8 +0,0 @@
-#ifndef WHEEL_H
-#define WHEEL_H
-
-#include "raylib.h"
-
-void wheel_draw(Vector2 center, float radius, float speed, int lines, Color color);
-
-#endif /* ifndef WHEEL_H */
